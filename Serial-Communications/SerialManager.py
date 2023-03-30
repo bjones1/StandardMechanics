b7b25657c046fcea2127855820c1d5f9124c930c
KKonaOG <kylerfarrar.gaming@gmail.com>
2023-03-30 05:13:18 -0500

    Tackling PR requests

--- Serial-Communications/SerialManager.py
+++ Serial-Communications/SerialManager.py
@@ -1,7 +1,32 @@
-import serial
+import serial # pip install pyserial (https://pypi.org/project/pyserial/)
 
 class JAISerial:
-    def __init__(self, serialPort, baudRate=9600, dataLength=serial.EIGHTBITS, stopBit=serial.STOPBITS_ONE, parity=serial.PARITY_NONE, XonXoff=0, timeout=None):
+# <div>The JAI-4000M Serial Manager is a class that allows for easy
+#     communication with the JAI SW-4000M-PMCL camera via serial
+#     communication.</div>
+# <div>Default Class Parameters were determined by the JAI SW-4000M-PMCL
+#     Command List <a
+#         href="../datasheets\Command-List-SW-4000M8000M-PMCL.pdf#page=3"
+#         target="_blank" rel="noopener">datasheet</a>.</div>
+    def __init__(self, serialPort, baudRWate=9600, dataLength=serial.EIGHTBITS, stopBit=serial.STOPBITS_ONE, parity=serial.PARITY_NONE, XonXoff=0, timeout=None):
+        """
+        Initializes a JAI-4000M Serial Manager on the specified serial port.
+        :param serialPort: The serial port to use (ex. "COM1")
+        :param baudRate: The baud rate to use in decimal (9600, 19200, 38400, 57600, 115200)
+        :param dataLength: The data length to use (serial.EIGHTBITS, serial.SEVENBITS, serial.SIXBITS, serial.FIVEBITS)
+        :param stopBit: The stop bit to use (serial.STOPBITS_ONE, serial.STOPBITS_ONE_POINT_FIVE, serial.STOPBITS_TWO)
+        :param parity: The parity to use (serial.PARITY_NONE, serial.PARITY_EVEN, serial.PARITY_ODD, serial.PARITY_MARK, serial.PARITY_SPACE)
+        :param XonXoff: The Xon/Xoff to use (0, 1)
+        :param timeout: The timeout to use in seconds (None, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        :type serialPort: str
+        :type baudRate: int
+        :type dataLength: int
+        :type stopBit: int
+        :type parity: int
+        :type XonXoff: int
+        :type timeout: int
+        """
+
         self.serialPort = serialPort
         self.baudRate = baudRate
         self.dataLength = dataLength
@@ -12,45 +37,76 @@ class JAISerial:
         self.__Open()
 
     def __Open(self):
+        """
+        Opens the serial port, this is called when the class is initialized and when the baud rate is changed, it is intended to be used internally and should not be called directly by the user.
+        """
+
         self.serialHandle = serial.Serial(self.serialPort, self.baudRate, self.dataLength, self.parity, self.stopBit, self.timeout, 0)
     
     def __Close(self):
+        """
+        Closes the serial port, this is called when the baud rate is changed and when the class is destroyed, it is intended to be used internally and should not be called directly by the user.
+        """
+
         self.serialHandle.close()
 
     def __ChangeBaudRate(self, baudRate):
+        """
+        Changes the baud rate of the serial port, this is called when the baud rate is changed by SetBaudRate, it is intended to be used internally and should not be called directly by the user.
+
+        :param baudRate: The baud rate to change to in decimal (9600, 19200, 38400, 57600, 115200)
+        :type baudRate: int
+        """
+
         self.__Close()
         self.serialHandle.baudrate = baudRate
         self.__Open()
 
     def __Write(self, command):
-        # Command should be a string such as "CBDRT=1" or "SBDRT?"
-        # It needs to be converted to bytes and have a carriage return and line feed added
+        """ 
+        Writes a command to the serial port, it is intended to be used internally and should not be called directly by the user.
+
+        :param command: The command to send to the serial port (ex. "CBDRT=1")
+        :type command: str
+        """
+
+        # The command is encoded to a byte array and then a carriage return and line feed are added in accordance with the JAI Serial Protocol (datasheets\Command-List-SW-4000M8000M-PMCL.pdf#page=3)
         self.serialHandle.write(command.encode() + b'\r\n')
 
     def __Read(self):
-        # Read the response until \r\n
+        """
+        Reads a response from the serial port, it is intended to be used internally and should not be called directly by the user. This function will block until a response is received or the timeout (see class constructor) is reached.
+        """
+
         response = self.serialHandle.readline()
         return response
 
     def GetSupportedBaudRates(self):
-        # Send the command
+        """
+        Gets the supported baud rates from the camera.
+        :return: A list of supported baud rates (9600, 19200, 38400, 57600, 115200) or an error message (01 Unknown Command!!, 02 Bad Parameters!!)
+        :rtype: list or str
+        """
+
+        # Send the command (datasheets\Command-List-SW-4000M8000M-PMCL.pdf#page=5)
         self.__Write("SBDRT?")
 
         # Read the Response
         response = self.__Read()
 
         # Expected Response Structure: b'SBDRT=<hexBaud>\r\n'
+        # (datasheets\Command-List-SW-4000M8000M-PMCL.pdf#page=5)
         # Where HexBaud is a hex value of the supported baud rates:
-        #     0x01 = 9600
-        #     0x03 = 19200
-        #     0x07 = 38400
-        #     0x0F = 57600
-        #     0x1F = 115200
+        #     00001 - 0x01 = 9600
+        #     00011 - 0x03 = 19200
+        #     00111 - 0x07 = 38400
+        #     01111 - 0x0F = 57600
+        #     11111 - 0x1F = 115200
 
         # If the response is b'01 Unknown Command!!\r\n' then the command was not recognized
         if response == b'01 Unknown Command!!\r\n':
             return "01 Unknown Command!!"
-        # If the response is b'02 Bad Parameters!!\r\n' then the parameters were not recognized
+        # If the response is b'02 Bad Parameters!!\r\n' then the parameters were not recognized (this should not happen, if it does it is indicative of bad serial communication)
         elif response == b'02 Bad Parameters!!\r\n':
             return "02 Bad Parameters!!"
         # If the response is b'SBDRT=<hexBaud>\r\n' then the command was recognized and the response is the supported baud rates
@@ -83,13 +139,22 @@ class JAISerial:
             return availableBaudRates
 
     def SetBaudRate(self, baudRate):
-        # If baudRate is the same as the current baud rate, then return "COMPLETE"
+        """
+        Sets the baud rate of the serial port via (CBDRT) command.
+        :param baudRate: The baud rate to change to in decimal (9600, 19200, 38400, 57600, 115200)
+        :type baudRate: int
+        :return: "COMPLETE" if the baud rate was changed successfully or an error message (01 Unknown Command!!, 02 Bad Parameters!!, FAILED)
+        :rtype: str
+        """
+
+        # If the baud rate is already set to the desired baud rate then return "COMPLETE", do not send the command again
+        # self.baudRate is maintained by the class and is updated when the baud rate is changed
         if baudRate == self.baudRate:
             return "COMPLETE"
 
         # Note: Need to confirm if baudRate needs to be decimal or hex
-        # This only supports decimal values for now
-
+        # This only supports decimal values for now (seemed to work in testing session with Trey Leonard)
+        # (datasheets\Command-List-SW-4000M8000M-PMCL.pdf#page=5)
         encodedBaudRate = 1 # 9600 by default
         
         if baudRate == 115200:
@@ -116,6 +181,8 @@ class JAISerial:
             return "02 Bad Parameters!!"
         # If the response is b'COMPLETE\r\n' then the command was recognized and the baud rate was changed
         elif response == b'COMPLETE\r\n':
+            # We must execute a confirmation command once the baud rate is changed to verify we are still communicating with the camera
+            # This protocol is described in the datasheet (datasheets\Command-List-SW-4000M8000M-PMCL.pdf#page=4)
             self.__ChangeBaudRate(baudRate)
             self.__Write("CBDRT=" + str(encodedBaudRate))
             response = self.__Read()
--- Serial-Communications/main.py
+++ Serial-Communications/main.py
@@ -1,7 +1,7 @@
 from SerialManager import JAISerial
 
 if __name__ == "__main__":
-    JAICamera = JAISerial("/dev/pts/4") # Change this to the correct serial port for the camera
+    JAICamera = JAISerial("COM1") # Change this to the correct serial port for the camera (likely COM1)
     JAICamera.GetSupportedBaudRates()
     JAICamera.SetBaudRate(115200)
     JAICamera.GetSupportedBaudRates()
