d08588441909259ba7bc5f4a04bbaf32b829d61a
Clark Hensley <clarkhensley@protonmail.com>
2023-04-05 23:49:53 -0500

    Add: general documentation; notes on why line-scan is infeasible right now

--- Image-acquisition-GUI/Python/grab_demo_testing.py
+++ Image-acquisition-GUI/Python/grab_demo_testing.py
@@ -16,21 +16,31 @@ grabDemo = GrabDemoDlg()
 #     it just needs the correct arguments provided.</p>
 #grabDemo.button_New_Click()
 
+# <p>Add the Sapera .dlls to the Path</p>
 sys.path.append("C:/Program Files/Teledyne DALSA/Sapera/Components/NET/Bin")
 
+# <p>We can reference the SapClassBasic and the Windows built-in forms to access
+#     everything we should need.</p>
 clr.AddReference("DALSA.SaperaLT.SapClassBasic")
 clr.AddReference("System.Windows.Forms")
 from System.Windows.Forms import *
 from DALSA.SaperaLT.SapClassBasic import *
 
+# <p>Here, we instantiate the .NET variables to use to define the acquisition
+# </p>
 m_Acquisition = SapAcquisition()
 m_Buffers = SapBuffer()
 #m_Xfer = SapAcqToBuf()
 m_View = SapView()
 m_IsSignalDetected = True
 
+# <p>This acConfigDlg reads config settings, shows the dialogue window</p>
 acConfigDlg = AcqConfigDlg(None, "", AcqConfigDlg.ServerCategory.ServerAcq)
-if (acConfigDlg.ShowDialog() == DialogResult.OK):
+dialog_result = acConfigDlg.ShowDialog()
+print(dialog_result)
+# <p>Set m_online based on if the Dialog populated correctly. This should work
+#     if the Sapera server exists</p>
+if (dialog_result == DialogResult.OK):
     m_online = True
 else:
     m_online = False
@@ -39,7 +49,15 @@ print(m_online)
 num_servers = SapManager.GetServerCount(SapManager.ResourceType.Acq)
 print(num_servers)
 
-
 m_ServerLocation = SapLocation()
 
-
+# <p>Most buttons in the dialogue operate based on m_Xfer to determine how/when
+#     to use them.</p>
+# <p>The Line-scan/Area-scan buttons instead operate directly off of
+#     m_Acquisition, and are only populated in the C++ version of the library.
+# </p>
+# <p>It would be possible to instantiate these buttons too, except that the
+#     dialogues they would need to populate do not exist in the common C#
+#     library of the demos.</p>
+# <p>Creating the Linescan option seems to require writing a C# dialogue for
+#     that</p>
